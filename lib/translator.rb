69b3749edb61230328c76db01f60cd97728c714c
Madeline Celis <madelinebcelis@gmail.com>
2018-08-02 16:35:09 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'pry'
 
 def load_library(filePath)
-  new_hash = { 'get_meaning' => {}, 'get_emotion' => {}}
+  new_hash = { 'get_meaning' => {}, 'get_emoticon' => {}}
   yaml_file = YAML.load_file(filePath)
   new_hash
 end
