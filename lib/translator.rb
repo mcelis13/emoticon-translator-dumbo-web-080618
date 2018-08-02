5382094c754d8926633ee255c07f263e78d6081f
Madeline Celis <madelinebcelis@gmail.com>
2018-08-02 16:32:04 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,14 +2,8 @@ require 'yaml'
 require 'pry'
 
 def load_library(filePath)
-  new_hash = {}
   yaml_file = YAML.load_file(filePath)
-
-  yaml_file.each do |key, symbols|
-    binding.pry
-  end#end of yaml_file.each
-
-  new_hash
+  
 end
 
 def get_japanese_emoticon
