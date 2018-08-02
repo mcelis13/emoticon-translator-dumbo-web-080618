cbe67ecf3e8a79314d3ac2aa1f2b8ea8d7959649
Madeline Celis <madelinebcelis@gmail.com>
2018-08-02 17:00:21 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(filePath)
   yaml_file.each do |key, emoticons|
       emoticons.each do |english_emoticon, japanese_emoticon|
         if new_hash['get_meaning'].has_key?(emoticons[1]) == false
-          new_hash['get_meaning'][emoticons[1]] => key}
+          new_hash['get_meaning'][emoticons[1]] = key
           new_hash['get_emoticon'] = {emoticons[0] => emoticons[1]}
         end# end of if new_hash
       end#end of emoticons.each
