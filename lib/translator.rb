1587dc396c0d7cc216754e25980fe82c91503f30
Madeline Celis <madelinebcelis@gmail.com>
2018-08-02 17:10:17 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(filePath, japanese_emoticon)
   if load_library(filePath)['get_emoticon'][japanese_emoticon]
     return load_library(filePath)['get_emoticon'][japanese_emoticon]
   else
-    'Sorry, that emoticon was not found'
+    return 'Sorry, that emoticon was not found'
   end
 end
 
